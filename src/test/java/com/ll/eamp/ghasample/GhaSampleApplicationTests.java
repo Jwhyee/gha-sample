312f5489b499d586020e1ee03da8bc0ce18895bd
Jwhyee <kawnsdud@naver.com>
2022-10-26 09:25:25 +0900

    Chore : Project Setting

---
+++ src/test/java/com/ll/eamp/ghasample/GhaSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.eamp.ghasample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GhaSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
